227946d61fed7f7d1acdd08754ac366f55d49bcb
conneroisu <conneroisu@outlook.com>
2025-03-09 16:33:01 -0500

    ``` chore: Update Obsidian workspace configuration ```

--- Vitis/eclipse_workspace/MP2-TPG/src/camera_app.c
+++ Vitis/eclipse_workspace/MP2-TPG/src/camera_app.c
@@ -13,14 +13,14 @@ int main()
 	// camera_loop(&camera_config);
 	while (1)
 	{
-   // TODO: Add switch from software to hardware mode!
-   for (int i = 0; i < 100; i++)
-   {
-      // XVprocSs_SetPictureBrightness(&proc_ss_RGB_YCrCb_444, (s32)i);
-      // XVprocSs_SetPictureContrast(&proc_ss_RGB_YCrCb_444, (s32)i);
-      set_contrast(&camera_config, i);
-      usleep(100000);
-   }
+		// TODO: Add switch from software to hardware mode!
+		for (int i = 0; i < 100; i++)
+		{
+			// XVprocSs_SetPictureBrightness(&proc_ss_RGB_YCrCb_444, (s32)i);
+			// XVprocSs_SetPictureContrast(&proc_ss_RGB_YCrCb_444, (s32)i);
+			set_contrast(&camera_config, i);
+			usleep(100000);
+		}
 	}
 
 	return 0;
