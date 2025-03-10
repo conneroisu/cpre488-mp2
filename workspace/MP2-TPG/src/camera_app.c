666149e87f3c9059f5ac3522314c026c2b464744
Parker <oparker@iastate.edu>
2025-03-10 16:13:27 -0500

    video working

--- workspace/MP2-TPG/src/camera_app.c
+++ workspace/MP2-TPG/src/camera_app.c
@@ -8,6 +8,12 @@
 camera_config_t camera_config;
 #define ROW_SIZE 1080
 #define COL_SIZE 1920
+#define NUM_IMAGES 100
+
+// Function prototypes
+void camera_config_init(camera_config_t *config);
+void camera_loop(camera_config_t *config);
+void camera_interface(camera_config_t *config, Xuint16 *rawBayer, int *StoreimageIndex, int *imageIndex, int *playMode);
 
 // Main function. Initializes the devices and configures VDMA
 int main() {
@@ -23,7 +29,7 @@ int main() {
     xil_printf("FMC-IMAGEON enabled successfully.\r\n");
 
     xil_printf("Starting camera loop...\r\n");
-    //camera_loop(&camera_config);
+    camera_loop(&camera_config);
 
     xil_printf("Program completed successfully.\r\n");
     return 0;
@@ -38,120 +44,173 @@ void camera_config_init(camera_config_t *config) {
     config->uBaseAddr_VITA_SPI = XPAR_ONSEMI_VITA_SPI_0_S00_AXI_BASEADDR;  // Device for configuring the Camera sensor
     config->uBaseAddr_VITA_CAM = XPAR_ONSEMI_VITA_CAM_0_S00_AXI_BASEADDR;  // Device for receiving Camera sensor data
 
-
     // Uncomment when using the TPG for Video input
 //    config->uBaseAddr_TPG_PatternGenerator = XPAR_V_TPG_0_S_AXI_CTRL_BASEADDR; // TPG Device
 
     config->uDeviceId_VTC_tpg   = XPAR_V_TC_0_DEVICE_ID;                        // Video Timer Controller (VTC) ID
     config->uDeviceId_VDMA_HdmiFrameBuffer = XPAR_AXI_VDMA_0_DEVICE_ID;         // VDMA ID
     config->uBaseAddr_MEM_HdmiFrameBuffer = XPAR_DDR_MEM_BASEADDR + 0x10000000; // VDMA base address for Frame buffers
-    config->uNumFrames_HdmiFrameBuffer = XPAR_AXIVDMA_0_NUM_FSTORES;            // NUmber of VDMA Frame buffers
+    config->uNumFrames_HdmiFrameBuffer = XPAR_AXIVDMA_0_NUM_FSTORES;            // Number of VDMA Frame buffers
     return;
 }
 
-//void camera_loop(camera_config_t *config) {
-//	Xuint32 parkptr;
-//	Xuint32 vdma_S2MM_DMACR, vdma_MM2S_DMACR;
+void camera_loop(camera_config_t *config) {
+    Xuint32 parkptr;
+    Xuint32 vdma_S2MM_DMACR, vdma_MM2S_DMACR;
+
+    xil_printf("Entering main SW processing loop\r\n");
+
+    int imageIndex = 0;
+    int playMode = 0;
+    int StoreimageIndex = 0;
+    Xuint16 *rawBayer = (Xuint16 *)malloc(1920 * 1080 * sizeof(Xuint16) * NUM_IMAGES);
+
+    UINTPTR SWIn = 0x41210000;
+    UINTPTR ButtonIn = 0x41200000;
+
+    xil_printf("Start processing frames!\r\n");
+
+    while((Xil_In32(SWIn) & 0x00000002) != 0x00000002) {
+        xil_printf("Loop!\r\n");
+        camera_interface(config, rawBayer, &StoreimageIndex, &imageIndex, &playMode);
+    }
+
+    xil_printf("Main SW processing loop complete!\r\n");
+    sleep(5);
+
+    // Uncomment when using TPG for Video input
+    // fmc_imageon_disable_tpg(config);
+
+    sleep(1);
+    return;
+}
+
+void camera_interface(camera_config_t *config, Xuint16 *rawBayer, int *StoreimageIndex, int *imageIndex, int *playMode) {
+    UINTPTR SWIn = 0x41210000;
+    UINTPTR ButtonIn = 0x41200000;
+	Xuint32 parkptr;
+	Xuint32 vdma_S2MM_DMACR, vdma_MM2S_DMACR;
+	int i, j;
+
+
+
+    volatile Xuint16 *pS2MM_Mem = (Xuint16 *)XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_S2MM_ADDR_OFFSET + XAXIVDMA_START_ADDR_OFFSET);
+    volatile Xuint16 *pMM2S_Mem = (Xuint16 *)XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_MM2S_ADDR_OFFSET + XAXIVDMA_START_ADDR_OFFSET + 4);
+
+    // Capture frame on middle button press
+    if ((Xil_In32(ButtonIn) & 0x00000010) == 0x00000010) {
+        if (*StoreimageIndex < NUM_IMAGES - 1) {
+            xil_printf("Capture\r\n");
+
+            // Wait for frame completion
+//            while (!(XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_SR_OFFSET) & 0x00000001)) {
+//                // Wait for frame completion
+//            }
+
+            // Copy the current frame to the rawBayer array
+            for (int i = 0; i < (1080 * 1920); i++) {
+                rawBayer[i + 1920 * 1080 * (*StoreimageIndex)] = pS2MM_Mem[i];
+            }
+
+
+            (*StoreimageIndex)++;
+        } else {
+            xil_printf("No more space for images\r\n");
+        }
+    }
+
+    // Playback mode
+    if ((Xil_In32(SWIn) & 0x00000001)) {
+        if (!(*playMode)) {
+            xil_printf("Play mode\r\n");
+            *playMode = 1;
+        }
+
+        // Navigate through images with left and right buttons
+        if ((Xil_In32(ButtonIn) & 0x00000001) == 0x00000001) {
+            if (*imageIndex < *StoreimageIndex - 1) {
+                (*imageIndex)++;
+            }
+        }
+        if ((Xil_In32(ButtonIn) & 0x00000002) == 0x00000002) {
+            if (*imageIndex > 0) {
+                (*imageIndex)--;
+            }
+        }
+
+        // Display the current image in playback mode
+
+
+    	parkptr = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET);
+    	parkptr &= ~XAXIVDMA_PARKPTR_READREF_MASK;
+    	parkptr &= ~XAXIVDMA_PARKPTR_WRTREF_MASK;
+    	parkptr |= 0x1;
+    	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET, parkptr);
+
+
+    	// Grab the DMA Control Registers, and clear circular park mode.
+    	vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET);
+    	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
+    	vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET);
+    	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
+
+
+
+        int count = 0;
+        while(count < 1){
+            for (int i = 0; i < (1080 * 1920); i++) {
+                pMM2S_Mem[i] = rawBayer[i + 1920 * 1080 * (*imageIndex)];
+            }
+            count++;
+        }
+        sleep(.002);
+
+
+    	vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET);
+    	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
+    	vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET);
+    	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
+
+//	    Xuint32 parkptr;
+//	    Xuint32 vdma_S2MM_DMACR, vdma_MM2S_DMACR;
+//	    // Grab the DMA parkptr, and update it to ensure that when parked, the S2MM side is on frame 0, and the MM2S side on frame 1
+//	    parkptr = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET);
+//	    parkptr &= ~XAXIVDMA_PARKPTR_READREF_MASK;
+//	    parkptr &= ~XAXIVDMA_PARKPTR_WRTREF_MASK;
+//	    parkptr |= 0x1;
+//	    XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET, parkptr);
+//	    // Grab the DMA Control Registers, and clear circular park mode.
+//	    vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET + XAXIVDMA_CR_OFFSET);
+//	    XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET + XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
+//	    vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET + XAXIVDMA_CR_OFFSET);
+//	    XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET + XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
 //
-//	xil_printf("Entering main SW processing loop\r\n");
 //
 //
-//	// Grab the DMA parkptr, and update it to ensure that when parked, the S2MM side is on frame 0, and the MM2S side on frame 1
-//	parkptr = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET);
-//	parkptr &= ~XAXIVDMA_PARKPTR_READREF_MASK;
-//	parkptr &= ~XAXIVDMA_PARKPTR_WRTREF_MASK;
-//	parkptr |= 0x1;
-//	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_PARKPTR_OFFSET, parkptr);
+//
+//	    sleep(2);
 //
 //
-//	// Grab the DMA Control Registers, and clear circular park mode.
-//	vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET);
-//	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
-//	vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET);
-//	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR & ~XAXIVDMA_CR_TAIL_EN_MASK);
 //
 //
-//	// Pointers to the S2MM memory frame and M2SS memory frame
-//	volatile Xuint16 *pS2MM_Mem = (Xuint16 *)XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_S2MM_ADDR_OFFSET+XAXIVDMA_START_ADDR_OFFSET);
-//	volatile Xuint16 *pMM2S_Mem = (Xuint16 *)XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_MM2S_ADDR_OFFSET+XAXIVDMA_START_ADDR_OFFSET+4);
-//
-//	uint32_t tempValue_R;
-//	uint32_t tempValue_G;
-//	uint32_t tempValue_B;
-//
-//    xil_printf("Start processing frames!\r\n");
-//    for (int j = 0; j < 1000; j++) {
-//    	for (int i = 0; i < (1080*1920); i+=2) {
-//
-//    		tempValue_R = 0x00000000;
-//			tempValue_G = 0x00000000;
-//			tempValue_B = 0x00000000;
-//
-//    		int r = i / 1920;
-//    		int c = i % 1920;
-//
-//    		/// bggr
-//    		// Red pixels
-//			if ((r % 2 == 1) && (c % 2 == 1)) {
-//				// Average blue neighbors (Diagonal)
-//				tempValue_B += ((pS2MM_Mem[(r-1) * COL_SIZE + (c-1)]) + (pS2MM_Mem[(r-1) * COL_SIZE + (c+1)]) + (pS2MM_Mem[(r+1) * COL_SIZE + (c-1)]) + (pS2MM_Mem[(r+1) * COL_SIZE + (c+1)])) / 4;
-//				// Average green neighbors (Cardinal)
-//				tempValue_G += ((pS2MM_Mem[(r-1) * COL_SIZE + c]) + (pS2MM_Mem[(r+1) * COL_SIZE + c]) + (pS2MM_Mem[r * COL_SIZE + (c - 1)]) + (pS2MM_Mem[r * COL_SIZE + (c+1)])) / 4;
-//				// red stays the same
-//				tempValue_R += (pS2MM_Mem[r * COL_SIZE + c]);
-//
-//			// Blue pixels
-//			} else if ((r % 2 == 0) && (c % 2 == 0)) {
-//				// blue stays the same
-//				tempValue_B += (pS2MM_Mem[r * COL_SIZE + c]);
-//				// Average green neighbors (Cardinal)
-//				tempValue_G += ((pS2MM_Mem[(r-1) * COL_SIZE + c]) + (pS2MM_Mem[(r+1) * COL_SIZE + c]) + (pS2MM_Mem[r * COL_SIZE + (c - 1)]) + (pS2MM_Mem[r * COL_SIZE + (c+1)])) / 4;
-//				// Average red neighbors (Diagonal)
-//				tempValue_R += ((pS2MM_Mem[(r-1) * COL_SIZE + (c-1)]) + (pS2MM_Mem[(r-1) * COL_SIZE + (c+1)]) + (pS2MM_Mem[(r+1) * COL_SIZE + (c-1)]) + (pS2MM_Mem[(r+1) * COL_SIZE + (c+1)])) / 4;
-//			} else {
-//					// Green stays same
-//					tempValue_G += (pS2MM_Mem[r * COL_SIZE + c]);
-//
-//					// Blue col: odd, even
-//					if ((r % 2 == 0) && (c % 2 == 1)) {
-//						// Average red neighbors (left/right)
-//						tempValue_R += ((pS2MM_Mem[(r-1) * COL_SIZE + c]) + (pS2MM_Mem[(r+1) * COL_SIZE + c])) / 2;
-//						// Average blue neighbors (up/down)
-//						tempValue_B += ((pS2MM_Mem[r * COL_SIZE + (c-1)]) + (pS2MM_Mem[r * COL_SIZE + (c+1)])) / 2;
-//					} else {
-//						// Average blue neighbors (left/right)
-//						tempValue_B += ((pS2MM_Mem[(r-1) * COL_SIZE + c]) + (pS2MM_Mem[(r+1) * COL_SIZE + c])) / 2;
-//						// Average red neighbors (up/down)
-//						tempValue_R += ((pS2MM_Mem[r * COL_SIZE + (c-1)]) + (pS2MM_Mem[r * COL_SIZE + (c+1)])) / 2;
-//					}
-//			}
-//
-//			uint8_t Y = ((0.183 * tempValue_R) + (0.614 * tempValue_G) + (0.062 * tempValue_B) + 16);
-//			uint8_t Cr = ((-0.101 * tempValue_R) + (-0.338 * tempValue_G) + (0.439 * tempValue_B) + 128);
-//			uint8_t Cb = ((0.439 * tempValue_R) + (-0.399 * tempValue_G) + (-0.040 * tempValue_B) + 128);
-//			// Set from YUV values.
-//			pMM2S_Mem[i] = (Cb << 8) | (Y & 0xFF);
-//			pMM2S_Mem[i + 1] = (Cr << 8) | ((Y & 0xFF00) >> 8);
-//    	}
-//    }
-//
-//	// Grab the DMA Control Registers, and re-enable circular park mode.
-//	vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET);
-//	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
-//	vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET);
-//	XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET+XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
-//
-//
-//	xil_printf("Main SW processing loop complete!\r\n");
-//
-//	sleep(5);
-//
-//	// Uncomment when using TPG for Video input
-//	// fmc_imageon_disable_tpg(config);
-//
-//	sleep(1);
-//
-//
-//	return;
-//
-//}
+//	    // Grab the DMA Control Registers, and re-enable circular park mode.
+//	    vdma_MM2S_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET + XAXIVDMA_CR_OFFSET);
+//	    XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_TX_OFFSET + XAXIVDMA_CR_OFFSET, vdma_MM2S_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
+//	    vdma_S2MM_DMACR = XAxiVdma_ReadReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET + XAXIVDMA_CR_OFFSET);
+//	    XAxiVdma_WriteReg(config->vdma_hdmi.BaseAddr, XAXIVDMA_RX_OFFSET + XAXIVDMA_CR_OFFSET, vdma_S2MM_DMACR | XAXIVDMA_CR_TAIL_EN_MASK);
+
+
+
+
+
+
+
+
+
+
+    } else {
+        *playMode = 0;
+    }
+}
+
+
